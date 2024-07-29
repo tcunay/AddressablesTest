f6600ec272dfa11abbf01e513b43e8e10c3c6bbc
Gevorg <gulkanyangk@mail.ru>
2024-07-29 14:17:30 +0500

    Fix

--- Assets/Editor/AddressablesTools/Build/AssetGroupExtensions.cs
+++ Assets/Editor/AddressablesTools/Build/AssetGroupExtensions.cs
@@ -4,16 +4,22 @@ using UnityEditor.AddressableAssets.Settings.GroupSchemas;
 
 namespace Editor.AddressablesTools.Build
 {
-  public static class AssetGroupExtensions
-  {
-    public static bool IsRemote(this AddressableAssetGroup group)
+    public static class AssetGroupExtensions
     {
-      BundledAssetGroupSchema schema = group.GetSchema<BundledAssetGroupSchema>();
-      if (schema == null)
-        return false;
+        public static bool IsRemote(this AddressableAssetGroup group)
+        {
+            BundledAssetGroupSchema schema = group.GetSchema<BundledAssetGroupSchema>();
+            
+            if (schema == null)
+                return false;
 
-      return schema.BuildPath.GetValue(AddressableAssetSettingsDefaultObject.Settings).Contains("http") ||
-             schema.LoadPath.GetValue(AddressableAssetSettingsDefaultObject.Settings).Contains("http");
+            return schema.BuildPath.IsRemote() ||
+                   schema.LoadPath.IsRemote();
+        }
+
+        private static bool IsRemote(this ProfileValueReference profile)
+        {
+            return profile.GetValue(AddressableAssetSettingsDefaultObject.Settings).Contains("http");
+        }
     }
-  }
 }
